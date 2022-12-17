d7a3a4fe1e9a5f0db889b25f503ac90769f81c94
Raviteja <61787804+RavitejaDarvemula@users.noreply.github.com>
2022-12-17 16:38:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let Btn = document.getElementById("btn");
+let inputEl = document.getElementById("input-el");
+let lengthEl = document.getElementById("length-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
+
+const meterToFeet = 3.281;
+const feetToMeter = 0.304;
+const literToGallon = 0.264;
+const gallonToLiter = 3.785;
+const kiloToPound = 2.204;
+const poundToKilo = 0.453;
+
+Btn.addEventListener("click", function () {
+  let baseValue = inputEl.value;
+  lengthEl.textContent = `${baseValue} meters = ${
+    baseValue * meterToFeet.toFixed(3)
+  } feet | ${baseValue} feet = ${baseValue * feetToMeter.toFixed(3)} meters`;
+  volumeEl.textContent = `${baseValue} liters = ${
+    baseValue * literToGallon.toFixed(3)
+  } gallons | ${baseValue} gallons = ${
+    baseValue * gallonToLiter.toFixed(3)
+  } liters`;
+  massEl.textContent = `${baseValue} kilos = ${
+    baseValue * kiloToPound
+  } pounds | ${baseValue} pounds = ${
+    baseValue * Number(poundToKilo).toFixed(3)
+  } kilos`;
+});
